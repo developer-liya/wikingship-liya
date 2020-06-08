781bc8e8de1005910cb66a211822d2c1a72801c7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-08 01:38:50 +0000

    Deploy developer-liya/wikingship-liya to github.com/developer-liya/wikingship-liya.git:gh-pages

---
+++ precache-manifest.9dcab87ea408af3fe46dfd457ca72777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8109932900d48f7d242c22a23007a995",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.9399dc8a014f2f5d333a.bundle.js"
+  },
+  {
+    "url": "runtime~main.9399dc8a014f2f5d333a.bundle.js"
+  },
+  {
+    "revision": "3e79aefef66e02b57d2f",
+    "url": "static/css/main.f77a9f49.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.9399dc8a014f2f5d333a.bundle.js"
+  },
+  {
+    "url": "vendors~main.9399dc8a014f2f5d333a.bundle.js.LICENSE.txt"
+  }
+]);
